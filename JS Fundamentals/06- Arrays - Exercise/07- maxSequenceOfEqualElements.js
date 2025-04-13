f4d180b3ff41830aade60115b86e557e5c9d1279
Denislav883 <yanev.denislav@gmail.com>
2025-04-13 13:12:49 +0300

    update

--- JS Fundamentals/06- Arrays - Exercise/07- maxSequenceOfEqualElements.js
+++ JS Fundamentals/06- Arrays - Exercise/07- maxSequenceOfEqualElements.js
@@ -1,4 +1,29 @@
 function maxSequenceOfEqualElements(arr){
-
-}
+        let bestElement = arr[0];
+        let bestLength = 1;
+    
+        let currentElement = arr[0];
+        let currentLength = 1;
+    
+        for (let i = 1; i < arr.length; i++) {
+            if (arr[i] === currentElement) {
+                currentLength++;
+            } else {
+                currentElement = arr[i];
+                currentLength = 1;
+            }
+    
+            if (currentLength > bestLength) {
+                bestLength = currentLength;
+                bestElement = currentElement;
+            }
+        }
+    
+        const output = [];
+        for (let i = 0; i < bestLength; i++) {
+            output.push(bestElement);
+        }
+    
+        console.log(output.join(' '));
+    }
 maxSequenceOfEqualElements();
